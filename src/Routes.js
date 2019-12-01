dd01cf69cb4e6cdf4851197b95d6451336711fd1
Michał Polak <235046@student.pwr.edu.pl>
2019-12-01 17:56:24 +0100

    added login

--- src/Routes.js
+++ src/Routes.js
@@ -2,9 +2,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import Home from './components/Home';
+import { Form } from 'antd';
+
+const WrappedRegistrationForm = Form.create({ name: 'register' })(Login);
 
 export default () =>
     <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/login" exact component={Login} />
+        <Route path="/login" exact component={WrappedRegistrationForm} />
     </Switch>;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,12 @@
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import { Form, Icon, Input, Button, Menu, Layout } from 'antd';
 import React from 'react';
+import 'antd/lib/form/style/css';
+import 'antd/lib/icon/style/css';
+import 'antd/lib/input/style/css';
+import 'antd/lib/button/style/css';
+import 'antd/lib/checkbox/style/css';
+
+const { Header, Content, Footer } = Layout;
 
 export default class Login extends React.Component {
     handleSubmit = e => {
@@ -14,41 +21,55 @@ export default class Login extends React.Component {
     render() {
         const { getFieldDecorator } = this.props.form;
         return (
-            <div>
-                <Form onSubmit={this.handleSubmit} className="login-form">
-                    <Form.Item>
-                        {getFieldDecorator('username', {
-                            rules: [{ required: true, message: 'Please input your username!' }],
-                        })(
-                            <Input
-                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                placeholder="Username"
-                            />,
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        {getFieldDecorator('password', {
-                            rules: [{ required: true, message: 'Please input your Password!' }],
-                        })(
-                            <Input
-                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                type="password"
-                                placeholder="Password"
-                            />,
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        {getFieldDecorator('remember', {
-                            valuePropName: 'checked',
-                            initialValue: true,
-                        })(<Checkbox>Remember me</Checkbox>)}
-                        <Button type="primary" htmlType="submit" className="login-form-button">
+            <Layout className="layout">
+                <Header>
+                    <div className="logo" style={{ float: 'left' }}>
+                        <h1 style={{ color: 'white', fontSize: '32px' }}>Online doctor</h1>
+                    </div>
+                    <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={['2']}
+                        style={{ lineHeight: '64px', float: 'right' }}
+                    >
+                        <Menu.Item key="1">Strona główna</Menu.Item>
+                        <Menu.Item key="2">Zarejestruj się</Menu.Item>
+                        <Menu.Item key="3">Zaloguj</Menu.Item>
+                    </Menu>
+                </Header>
+                <Content>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100vhs' }}></div>
+                        <Form.Item>
+                            {getFieldDecorator('username', {
+                                rules: [{ required: true, message: 'Please input your username!' }],
+                            })(
+                                <Input
+                                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    placeholder="Username"
+                                />,
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator('password', {
+                                rules: [{ required: true, message: 'Please input your Password!' }],
+                            })(
+                                <Input
+                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    type="password"
+                                    placeholder="Password"
+                                />,
+                            )}
+                        </Form.Item>
+                        <Button style={{ float: 'none' }} type="primary" htmlType="submit" className="login-form-button">
                             Log in
-                </Button>
-                        Or <a href="">register now!</a>
-                    </Form.Item>
-                </Form>
-            </div>
+                    </Button>
+                        <div>
+                            Or <a href="">register now!</a>
+                        </div>
+                    </Form>
+                </Content>
+            </Layout>
         );
     }
 }
