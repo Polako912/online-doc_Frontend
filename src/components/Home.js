4999e8b6facd62599e767099bf593326980e49d1
fazerito <gervvaz@gmail.com>
2019-12-01 17:14:30 +0100

    Added home page layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,49 @@
-import { Layout, Menu, Breadcrumb } from 'antd';
-
+import React from 'react';
+import { Layout, Menu, Breadcrumb, Input } from 'antd';
+import 'antd/dist/antd.css';
+import './Home.css'
 const { Header, Content, Footer } = Layout;
+const { Search } = Input;
+class Home extends React.Component {
+    render() {
+        return (
+            <Layout className="layout">
+                <Header>
+                    <div className="logo" style={{ float: 'left' }}>
+                        <h1 style={{ color: 'white', fontSize: '32px' }}>Online doctor</h1>
+                    </div>
+                    <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={['2']}
+                        style={{ lineHeight: '64px', float: 'right' }}
+                    >
+                        <Menu.Item key="1">Strona główna</Menu.Item>
+                        <Menu.Item key="2">Zarejestruj się</Menu.Item>
+                        <Menu.Item key="3">Zaloguj</Menu.Item>
+                    </Menu>
+                </Header>
+                <Content style={{ padding: '0 50px' }}>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
 
-ReactDOM.render(
-  <Layout className="layout">
-    <Header>
-      <div className="logo" />
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={['2']}
-        style={{ lineHeight: '64px' }}
-      >
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
-      </Menu>
-    </Header>
-    <Content style={{ padding: '0 50px' }}>
-      <Breadcrumb style={{ margin: '16px 0' }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
-      </Breadcrumb>
-      <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
-    </Content>
-    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-  </Layout>,
-  mountNode,
-);
+                    </Breadcrumb>
+                    <div style={{ background: '#fff', padding: 24, minHeight: 600 }}>
+                        <Search
+                            className="search"
+                            placeholder="specjalizacja/nazwisko"
+                            onSearch={value => console.log(value)}
+                        />
+                        <Search
+                            className="search"
+                            placeholder="miasto"
+                            onSearch={value => console.log(value)}
+                        />
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+            </Layout>
+        )
+    }
+}
+
+export default Home
